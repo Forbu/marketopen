aa72c5f4eda76c57ac30aa7735139fc3234a5fd4
Adrien B <adrien.bufort@supaero.isae.fr>
2017-09-01 23:52:43 +0200

    express initialisation

---
+++ marketopenUI/app.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Part where we see all the product
+app.post('/product',function(req, res){
+  var product = req.body.id;
+
+  // make the request for the product in question
+
+
+});
+
+// Part where we see all the sellers
+app.post('/seller',function(req, res){
+  var address = req.body.address;
+
+  // make the request for the stuff about the seller
+
+
+});
+
+// Part where we see all the buyers
+app.post('/buyer',function(req, res){
+  var address = req.body.address;
+
+  // make the the request to see the buyer
+
+});
+
+
+
+module.exports = app;
